e23c90cac83140156b6ad5ef980083605e675193
teagh82 <teagh8237@gmail.com>
2020-05-06 22:00:02 +0900

    기본과제 1

---
+++ SOPT_2/app/src/main/java/com/example/sopt_2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.sopt_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_viewPager.adapter = MainPagerAdapter(supportFragmentManager)
+        main_viewPager.offscreenPageLimit = 2
+        main_viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                //내비게이션 메뉴 아이템 체크
+                bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+
+        bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.menu_home -> main_viewPager.currentItem = 0
+                R.id.menu_book -> main_viewPager.currentItem = 1
+                R.id.menu_person -> main_viewPager.currentItem = 2
+            }
+            true
+        }
+    }
+}
